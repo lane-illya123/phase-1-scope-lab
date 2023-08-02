96005c767553a8f49db02ef7c94ce42d07215c20
illyalane <lane_illya@yahoo.com>
2023-08-02 08:49:13 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+const leastFavoriteCustomer = "john"
+
+//uppercasr customerName
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+//set best customer
+function setBestCustomer(){
+bestCustomer = "not bob";
+}
+//overwrite best customer
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer ="larry"
+}
